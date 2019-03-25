d98f9a042425edd2f79bdcfd4a2a01558823b610
Lukas Singer <lukassinger@Lukass-MacBook-Pro.local>
2019-03-25 07:58:33 -0700

    Implement basic server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3003, function() {
+  console.log('listening on port 3003!');
+});
 
 
