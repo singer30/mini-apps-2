c878aa60bbb848613bf33dd65ecd449c5b04dea4
Lukas Singer <lukassinger@Lukass-MacBook-Pro.local>
2019-03-25 08:56:11 -0700

    Implement webpack config

--- challenge_1/webpack.config.js
+++ challenge_1/webpack.config.js
@@ -1,23 +1,29 @@
 var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
+var SRC_DIR = path.join(__dirname, '/client');
 var DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-       }
-      }
-    ]
-  }
-};
+    "mode": "development",
+    "entry": "./client/index.jsx",
+    "output": {
+        "path": __dirname+'/public',
+        "filename": "bundle.js"
+    },
+    "module": {
+        "rules": [
+            {
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader",
+                    "options": {
+                        "presets": [
+                            "env",
+                            "react"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
